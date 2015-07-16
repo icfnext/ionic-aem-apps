159f67c85b0364c744efa01b4e78196b8c143ee4
pmichelotti <pmichelotti@citytechinc.com>
2015-07-16 10:37:46 -0500

    Updates to side menu state to allow configurability of menu side

---
+++ ionic-aem-apps-core/src/main/java/com/icfi/aem/apps/ionic/core/components/page/sidemenusapplicationstate/SideMenusApplicationState.java
@@ -0,0 +1,39 @@
+package com.icfi.aem.apps.ionic.core.components.page.sidemenusapplicationstate;
+
+import com.citytechinc.aem.bedrock.core.components.AbstractComponent;
+import com.citytechinc.cq.component.annotations.Component;
+import com.citytechinc.cq.component.annotations.DialogField;
+import com.citytechinc.cq.component.annotations.Option;
+import com.citytechinc.cq.component.annotations.widgets.DateTime;
+import com.citytechinc.cq.component.annotations.widgets.Selection;
+
+import java.util.List;
+
+@Component("Side Menus Application State")
+public class SideMenusApplicationState extends AbstractComponent {
+
+    public static final String LEFT_MENU_SIDE = "left";
+    public static final String RIGHT_MENU_SIDE = "right";
+
+    @DialogField
+    @DateTime
+    @Selection(
+            type = Selection.CHECKBOX,
+            options = {
+                    @Option(text = "Left", value = SideMenusApplicationState.LEFT_MENU_SIDE),
+                    @Option(text = "Right", value = SideMenusApplicationState.RIGHT_MENU_SIDE)
+            }
+    )
+    public List<String> getMenuSides() {
+        return getAsList("menuSides", String.class);
+    }
+
+    public boolean isUsesLeftSideMenu() {
+        return getMenuSides().contains(LEFT_MENU_SIDE);
+    }
+
+    public boolean isUsesRightSideMenu() {
+        return getMenuSides().contains(RIGHT_MENU_SIDE);
+    }
+
+}
